b84c2b33f4fdc9ab89eee411073cb1fe1ac9adf5
Cam Saül <cammsaul@gmail.com>
2017-01-13 17:38:06 -0800

    Minor cleanup :shower:

--- src/metabase/middleware.clj
+++ src/metabase/middleware.clj
@@ -203,7 +203,7 @@
          content-security-policy-header
          #_(public-key-pins-header)
          (when-not allow-iframes?
-           {"X-Frame-Options"                   "DENY"})      ; Tell browsers not to render our site as an iframe (prevent clickjacking)
+           {"X-Frame-Options"                 "DENY"})        ; Tell browsers not to render our site as an iframe (prevent clickjacking)
          {"X-XSS-Protection"                  "1; mode=block" ; Tell browser to block suspected XSS attacks
           "X-Permitted-Cross-Domain-Policies" "none"          ; Prevent Flash / PDF files from including content from site.
           "X-Content-Type-Options"            "nosniff"}))    ; Tell browser not to use MIME sniffing to guess types of files -- protect against MIME type confusion attacks
@@ -215,7 +215,7 @@
     (let [response (handler request)]
       (update response :headers merge (cond
                                         (api-call? request) (api-security-headers)
-                                        (public? request)   (html-page-security-headers :allow-iframes? true)
+                                        (public? request)   (html-page-security-headers, :allow-iframes? true)
                                         (index? request)    (html-page-security-headers))))))
 
 
